176880b6a670db881be8add426972e7cd1e2b5c0
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-08-02 14:39:42 -0700

    fetch posts set up in post_index component

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {fetchPosts} from '../actions'
+import _ from 'lodash'
 
 class PostsIndex extends Component {
 //this is called immediately after the component has shown up inside the DOM. Good for something you want to check exactly one time.
@@ -9,14 +10,36 @@ class PostsIndex extends Component {
     //this kicks off our data loading process
     this.props.fetchPosts()
   }
+//here we'll map over our list of posts and make a li with each one
+//remember, we're dealing with an object now so we need lodash's map function which has the ability to deal with objects
+  renderPosts(){
+    return _.map(this.props.posts, post => {
+      return (
+        <li className='list-group-item' key={post.id}>
+          {post.title}
+        </li>
+      )
+    })
+  }
+//returns an array
 
   render(){
     return (
-      <div>Posts Index</div>
+      <div>
+        <h3>Posts</h3>
+        <ul className='list-group'>
+          {this.renderPosts()}
+        </ul>
+      </div>
     )
   }
 }
 
-export default connect(null, {fetchPosts})(PostsIndex)
+//whenevr we want to consume anything from application level state, we always define the mapStateToProps function
+function mapStateToProps(state){
+  return {posts: state.posts}
+}
+
+export default connect(mapStateToProps, {fetchPosts})(PostsIndex)
 
 //wiring up an action creator with this syntax, instead of mapDispatchToProps, is completely identical in nature, so we still have access to this.props.fetchPosts inside of our component. However, there may be times when you want to have a separate function in case you want to do more logic
