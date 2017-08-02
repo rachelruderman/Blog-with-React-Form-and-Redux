f5163f6b252988c14a844d0db517e150ee6fa04a
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-08-02 14:27:24 -0700

    alternative way to use react-redux

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,6 +1,15 @@
 import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import {fetchPosts} from '../actions'
 
 class PostsIndex extends Component {
+//this is called immediately after the component has shown up inside the DOM. Good for something you want to check exactly one time.
+//React always eagerly loads itself as soon as it can. We're always going to end up with the component being loaded one time before we have the info fetched
+  componentDidMount(){
+    //this kicks off our data loading process
+    this.props.fetchPosts()
+  }
+
   render(){
     return (
       <div>Posts Index</div>
@@ -8,4 +17,6 @@ class PostsIndex extends Component {
   }
 }
 
-export default PostsIndex
+export default connect(null, {fetchPosts})(PostsIndex)
+
+//wiring up an action creator with this syntax, instead of mapDispatchToProps, is completely identical in nature, so we still have access to this.props.fetchPosts inside of our component. However, there may be times when you want to have a separate function in case you want to do more logic
