480d68c31e7766c3d62c82f51ad84113c27e61c3
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-08-02 16:47:08 -0700

    set up posts_new component and routing

--- src/index.js
+++ src/index.js
@@ -2,19 +2,24 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
 //BrowserRouter says: I want react router to look at the entire url when deciding what to display on the screen. It's a piece of code that interacts with the history library
 import promise from 'redux-promise';
 import reducers from './reducers';
 import PostsIndex from './components/posts_index'
+import PostsNew from './components/posts_new'
 
 const createStoreWithMiddleware = applyMiddleware(promise)(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
+      {/* //switch renders the first route that matches the current url. So you want to put your most specific routes at the top of the list  */}
       <div>
-        <Route path='/' component={PostsIndex} />
+        <Switch>
+          <Route path='/posts/new' component={PostsNew} />
+          <Route path='/' component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
