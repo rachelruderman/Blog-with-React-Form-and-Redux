11b62d895fa3e09112c9e4e9d2f01f4e2baa4eb8
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-08-02 10:05:04 -0700

    added routes

--- src/index.js
+++ src/index.js
@@ -5,27 +5,16 @@ import { createStore, applyMiddleware } from 'redux';
 import {BrowserRouter, Route} from 'react-router-dom';
 //BrowserRouter says: I want react router to look at the entire url when deciding what to display on the screen. It's a piece of code that interacts with the history library
 
-import App from './components/app';
 import reducers from './reducers';
+import PostsIndex from './components/posts_index'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-class Hello extends React.Component{
-  render(){ return <div>Hello</div> }
-}
-
-class Goodbye extends React.Component{
-  render(){ return <div>Goodbye</div> }
-}
-
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    {/* <App /> */}
     <BrowserRouter>
       <div>
-      {/* if a user goes to this path, i want to show this component */}
-        <Route path='/hello' component={Hello}/>
-        <Route path='/goodbye' component={Goodbye}/>
+        <Route path='/' component={PostsIndex} />
       </div>
     </BrowserRouter>
   </Provider>
