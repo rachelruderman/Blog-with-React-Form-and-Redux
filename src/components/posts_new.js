89b1cadb869ed6577b21c9c5f8dc2951bf65e7c8
Rachel Ruderman <raquel.rudermano@gmail.com>
2017-08-02 18:22:47 -0700

    added validation

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -14,13 +14,23 @@ class PostsNew extends Component {
           {...field.input}
         //this object contains a bunch of event handlers and props. The ... says ok, this is an object here, and I want all of the dif properties in this object to be communicated as props to the input tag
         />
+        {field.meta.error}
       </div>
+      //this meta.error property is automatically added to the field object from our validate function
     )
   }
 
+  onSubmit(values){
+    console.log(values)
+  }
+
   render(){
+    //connect helper was used to wire up additional properties to the component. reduxForm does the same, and gives it the prop of handleSubmit.
+    //handleSubmit takes a look at the form, once redux decides that everything is a-okay and ready to be submitted, it passes it on to the callback function defined by the developer
+    const {handleSubmit} = this.props
+
     return (
-      <form>
+      <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
         <Field
           label='Title'
           name='title'
@@ -29,17 +39,43 @@ class PostsNew extends Component {
           component={this.renderField}
         />
         <Field
-          label='Tags'
-          name='tags'
+          label='Categories'
+          name='categories'
           component={this.renderField}
         />
+        <Field
+          label='Post Content'
+          name='content'
+          component={this.renderField}
+        />
+        <button type='submit' className='btn btn-primary'>Submit</button>
       </form>
     )
   }
 }
+//this will be called automatically for us at certain points during the form's lifecycle; ie. when teh user tries to submit the form. It's given a single argument, an object, that includes all the values the user has entered into the form
+function validate(values){
+  //the name property and the property in the validate function must be identical for the errors to show up
+  //the errors object starts out as completely empty
+  const errors = {}
+  //then we look at a property on the values object
+  if(!values.title || values.title.length < 3){
+  //add a property to the errors object
+    errors.title = 'Enter a title that is at least 3 characters!'
+  }
+  if(!values.categories){
+    errors.categories = 'Enter some categories'
+  }
+  if(!values.content){
+    errors.content = 'Enter some content please'
+  }
+  //if the validate function returns an empty object, redux submits the form
+  return errors;
+}
 
 //redux form takes a single argument, which is a function
 export default reduxForm({
+  validate,
   form: 'PostsNewForm'
 })(PostsNew)
 
